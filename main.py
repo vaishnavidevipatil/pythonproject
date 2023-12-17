a4fb4c4878fd864b916b548e0731b332a15c912e
vaishnavidevipatil <145427408+vaishnavidevipatil@users.noreply.github.com>
2023-12-17 20:27:02 +0530

    get api added

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 # app.py
+import mysql.connector
+import pandas as pd
+from flask import Flask, jsonify
+app = Flask(__name__)
 
-from flask import render_template
-import connexion
+@app.route('/api/get/students/data', methods=['GET'])
+def get_data():
+    # Create a connection object
+    my_conn = mysql.connector.connect(host="localhost", user="root", password="Mysql@2024", database="mysqltut")
+    
+    df = pd.read_sql("SELECT * FROM mysqltut.students",con= my_conn)
+    my_conn.close()
+    # Replace this with the actual data you want to return
+    data=df.to_dict("records")
+    return jsonify(data)
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
-app = connexion.App(__name__, specification_dir="./")
-app.add_api("swagger.yml")
-
-
-
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
